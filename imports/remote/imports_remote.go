6cd07f264cd354bc67199e5fbde9813771d4f23f
Vladimir Vivien <vladimir.vivien@gmail.com>
2016-04-29 17:39:16 -0500

    VirtualBox executor and driver impl
        This is faith port of the virtualbox driver from Rex-Ray.
        It also includes an executor implmentation that
        includes a lightweight soap client.  Other changes include
        unit tests for all implmented methods (see issue #20)

--- imports/remote/imports_remote.go
+++ imports/remote/imports_remote.go
@@ -3,5 +3,6 @@ package remote
 import (
 	// import to load
 	_ "github.com/emccode/libstorage/drivers/storage/mock"
+	_ "github.com/emccode/libstorage/drivers/storage/vbox"
 	_ "github.com/emccode/libstorage/drivers/storage/vfs/storage"
 )
