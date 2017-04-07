cecb9ac31336c0b7abb1ac20c84958c82fddf727
Mathieu Velten <mathieu.velten@cern.ch>
2017-04-07 11:56:43 +0200

    OpenStack: rework the code to get the instance id
    The code to read from cloud-init doesn't work all the time,
    sometimes the returned id is in the form opentack-XXXXX for whatever reason.
    Switch this code path to mounting and reading the cloud config drive instead.

--- drivers/storage/openstack/executor/openstack_executor.go
+++ drivers/storage/openstack/executor/openstack_executor.go
@@ -8,19 +8,26 @@ import (
 
 	"github.com/codedellemc/libstorage/api/registry"
 	"github.com/codedellemc/libstorage/api/types"
+	"github.com/codedellemc/libstorage/api/utils"
 	"github.com/codedellemc/libstorage/drivers/storage/openstack"
+	// the next import initializes and registers the OS drivers
+	_ "github.com/codedellemc/libstorage/imports/local"
 
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"os/exec"
+	"path/filepath"
 	"regexp"
+	"runtime"
 	"strings"
+	"time"
 )
 
 type driver struct {
-	config gofig.Config
+	config   gofig.Config
+	osDriver types.OSDriver
 }
 
 func init() {
@@ -33,6 +40,15 @@ func newDriver() types.StorageExecutor {
 
 func (d *driver) Init(ctx types.Context, config gofig.Config) error {
 	d.config = config
+
+	var err error
+	if d.osDriver, err = registry.NewOSDriver(runtime.GOOS); err != nil {
+		return err
+	}
+	if err = d.osDriver.Init(ctx, config); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -40,24 +56,18 @@ func (d *driver) Name() string {
 	return openstack.Name
 }
 
-// InstanceID returns the local instance ID for the test
-func InstanceID(config gofig.Config) (*types.InstanceID, error) {
-	d := newDriver()
-	d.Init(nil, config)
-	return d.InstanceID(nil, nil)
-}
-
-func (d *driver) InstanceID(
-	ctx types.Context,
-	opts types.Store) (*types.InstanceID, error) {
-
-	uuid, metadataServerErr := getInstanceIDFromMetadataServer()
-	if metadataServerErr != nil {
-		uuid, cloudInitFileErr = getInstanceIDFromCloudInitFile()
-		if cloudInitFileErr != nil {
-			uuid, dmidecodeErr = getInstanceIDWithDMIDecode()
-			if dmidecodeErr != nil {
-				return nil, fmt.Errorf("%v ; %v ; %v", cloudInitFileErr, metadataServerErr, dmidecodeErr)
+func (d *driver) InstanceID(ctx types.Context, opts types.Store) (*types.InstanceID, error) {
+	fields := map[string]interface{}{}
+	uuid, err := getInstanceIDFromConfigDrive(ctx, d)
+	if err != nil {
+		fields["configDrive"] = err
+		uuid, err = getInstanceIDFromMetadataServer()
+		if err != nil {
+			fields["metadataServer"] = err
+			uuid, err = getInstanceIDWithDMIDecode()
+			if err != nil {
+				fields["dmidecode"] = err
+				return nil, goof.WithFields(fields, "unable to get InstanceID from any sources")
 			}
 		}
 	}
@@ -67,33 +77,9 @@ func (d *driver) InstanceID(
 	return iid, nil
 }
 
-func getInstanceIDFromCloudInitFile() (string, error) {
-	const instanceIDFile = "/var/lib/cloud/data/instance-id"
-	idBytes, err := ioutil.ReadFile(instanceIDFile)
-	if err != nil {
-		return "", goof.WithFieldE("file", instanceIDFile, "error reading file", err)
-	}
-
-	instanceID := string(idBytes)
-	instanceID = strings.TrimSpace(instanceID)
-
-	return instanceID, nil
-}
-
-func getInstanceIDFromMetadataServer() (string, error) {
-	const metadataURL = "http://169.254.169.254/openstack/latest/meta_data.json"
-	resp, err := http.Get(metadataURL)
-	if err != nil {
-		return "", goof.WithError("error getting metadata from "+metadataURL, err)
-	}
-	defer resp.Body.Close()
-
-	data, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", goof.WithError("io error reading metadata", err)
-	}
+func parseUUID(metadata []byte) (string, error) {
 	var decodedJSON interface{}
-	err = json.Unmarshal(data, &decodedJSON)
+	err := json.Unmarshal(metadata, &decodedJSON)
 	if err != nil {
 		return "", goof.WithError("error unmarshalling metadata", err)
 	}
@@ -109,21 +95,97 @@ func getInstanceIDFromMetadataServer() (string, error) {
 	return uuid, nil
 }
 
-func getInstanceIDWithDMIDecode() (string, error) {
-	cmd := exec.Command("dmidecode", "-t", "system")
-	cmdOut, err := cmd.Output()
-
+func execCommand(cmd string, args ...string) (string, error) {
+	command := exec.Command(cmd, args...)
+	out, err := command.Output()
 	if exiterr, ok := err.(*exec.ExitError); ok {
 		stderr := string(exiterr.Stderr)
-		ctx.WithError(
-			exiterr,
-		).WithField(
-			"stderr", stderr,
-		).Error("error calling dmidecode")
-		return nil,
-			goof.Newf("error calling dmidecode: %s", stderr)
+		return "", goof.WithFieldE("stderr", stderr, "execute command failed", err)
+	} else if err != nil {
+		return "", goof.WithError("execute command failed", err)
 	}
-	return nil, goof.WithError("error calling dmidecode", err)
+	return string(out), nil
+}
+
+// the code of getInstanceIDFromConfigDrive is mostly copied from k8s OpenStack driver
+// https://github.com/kubernetes/kubernetes/blob/master/pkg/cloudprovider/providers/openstack/metadata.go
+// Copyright to the original authors (Apache license)
+
+// Config drive is defined as an iso9660 or vfat (deprecated) drive
+// with the "config-2" label.
+// http://docs.openstack.org/user-guide/cli-config-drive.html
+const configDriveLabel = "config-2"
+const configDrivePath = "openstack/latest/meta_data.json"
+
+func getInstanceIDFromConfigDrive(ctx types.Context, d *driver) (string, error) {
+	// Try to read instance UUID from config drive.
+	dev := "/dev/disk/by-label/" + configDriveLabel
+	if _, err := os.Stat(dev); os.IsNotExist(err) {
+		cmdOut, err := execCommand(
+			"blkid", "-l",
+			"-t", "LABEL="+configDriveLabel,
+			"-o", "device",
+		)
+
+		if err != nil {
+			return "", goof.WithError("Unable to run blkid", err)
+		}
+		dev = strings.TrimSpace(string(cmdOut))
+	}
+
+	mntdir, err := ioutil.TempDir("", "configdrive")
+	if err != nil {
+		return "", err
+	}
+	defer os.Remove(mntdir)
+
+	mountOpts := types.DeviceMountOpts{
+		FsType:       "iso9660",
+		MountOptions: "ro",
+	}
+	err = d.osDriver.Mount(ctx, dev, mntdir, &mountOpts)
+	if err != nil {
+		mountOpts.FsType = "vfat"
+		err = d.osDriver.Mount(ctx, dev, mntdir, &mountOpts)
+	}
+	if err != nil {
+		return "", goof.WithFieldE("device", dev, "error mounting configdrive", err)
+	}
+	defer d.osDriver.Unmount(ctx, mntdir, utils.NewStore())
+
+	metadataBytes, err := ioutil.ReadFile(filepath.Join(mntdir, configDrivePath))
+	if err != nil {
+		return "", goof.WithError("error reading metadata file on config drive", err)
+	}
+
+	return parseUUID(metadataBytes)
+}
+
+func getInstanceIDFromMetadataServer() (string, error) {
+	const metadataURL = "http://169.254.169.254/openstack/latest/meta_data.json"
+	httpClient := http.Client{
+		Timeout: time.Duration(5 * time.Second),
+	}
+
+	resp, err := httpClient.Get(metadataURL)
+	if err != nil {
+		return "", goof.WithFieldE("url", metadataURL, "error getting metadata from server", err)
+	}
+	defer resp.Body.Close()
+
+	metadataBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", goof.WithError("io error reading metadata", err)
+	}
+
+	return parseUUID(metadataBytes)
+}
+
+func getInstanceIDWithDMIDecode() (string, error) {
+	cmdOut, err := execCommand("dmidecode", "-t", "system")
+	if err != nil {
+		return "", goof.WithError("error calling dmidecode", err)
+	}
 
 	rp := regexp.MustCompile("UUID:(.*)")
 	uuid := strings.Replace(rp.FindString(string(cmdOut)), "UUID: ", "", -1)
